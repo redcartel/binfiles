0e43683bb9f568ac5e9da8368434b32a5dc2e4e8
Carter Adams <carteradams@gmail.com>
2018-10-18 10:43:53 -0400

    state of bin before new laptop

--- alias.sh
+++ alias.sh
@@ -1,5 +1,8 @@
+# source ~/bin/alias.sh  # copy to .bashrc and .zshrc
+
 alias python=python3
 alias pip=pip3
 # alias icd='cd /home/redcartel/cdrive/byte/instruction/icd-example'
 alias venv='source *venv*/bin/activate'
-alias cdwk="cd \$(pwd -P)"
+alias cdwk='cd $(readlink ~/wk)'
+alias wk='rm -f ~/wk && ln -s $(pwd -P) ~/wk'
