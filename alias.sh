7a03329a9ae57cc57dc2179b455592271fcfeac0
Carter A <carteradams@protonmail.com>
2019-04-17 14:35:17 -0400

    pre-backup

--- alias.sh
+++ alias.sh
@@ -4,14 +4,19 @@ export PATH="/home/redcartel/binfiles:/home/redcartel/.local/bin:/home/redcartel
 
 mkdir -p ~/w
 
-alias python=python3
-alias pip=pip3
-alias virtualenv='python3 -m virtualenv'
+# alias virtualenv='python3 -m virtualenv'
 alias venv='if [ ! -d "venv" ]; then virtualenv venv; source venv/bin/activate; elif [ -z "$VIRTUAL_ENV" ]; then source venv/bin/activate; else deactivate; fi'
+
+alias npm-exec='PATH=$(npm bin):$PATH'
+
 alias suds='history | grep "sudo apt" | grep "install" | grep -v "grep"'
+
+alias run='bash run.sh'
+alias utest='bash test.sh'
 
 alias wk='rm -f ~/wk && ln -s $(pwd -P) ~/wk && echo "$(pwd -P)" >> ~/var/log/wk.log'
 alias cdwk='cd $(readlink ~/wk)'
+alias lswk='tail -n 22 ~/var/log/wk.log'
 
 alias w1='cd $(_cdwn.sh 1)'
 alias w2='cd $(_cdwn.sh 2)'
@@ -23,5 +28,7 @@ alias w7='cd $(_cdwn.sh 7)'
 alias w8='cd $(_cdwn.sh 8)'
 alias w9='cd $(_cdwn.sh 9)'
 
+
 alias tdn='vim ~/etc/todo-now.txt'
 alias tdt='vim ~/etc/todo-then.txt'
+alias tdl='vim ~/etc/todo-life.txt'
